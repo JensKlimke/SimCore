f20281c62d511c0b142d70c8db7f763d84b27aa2
Jens Klimke <jens.klimke@rwth-aachen.de>
2019-04-15 23:04:09 +0200

    remove driver model (to another repository)

--- test/main.cpp
+++ test/main.cpp
@@ -1,14 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "SimTest.h"
-#include "VehicleModelTest.h"
 #include "AlgoTest.h"
-#include "DMAlgoTest.h"
-#include "agent_tests/AMT_PriInputs.h"
-#include "agent_tests/AMT_VelControl.h"
-#include "agent_tests/AMT_VelChoice.h"
-#include "agent_tests/AMT_Stop.h"
-#include "agent_tests/AMT_Follow.h"
 
 int main(int argc, char **argv) {
 
