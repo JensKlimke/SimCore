9047a2586a3ba3cf13c53eecaf5937f4a47f9915
Jens Klimke <jens.klimke@rwth-aachen.de>
2019-06-12 09:04:54 +0200

    Added Boost (doesn't work yet completely)

--- cmake/AddBoost.cmake
+++ cmake/AddBoost.cmake
@@ -6,28 +6,24 @@
 
 message("Downloading boost ...")
 
-if (CMAKE_VERSION VERSION_LESS 3.11)
-    set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
+include(FetchContent)
 
-    include(DownloadProject)
-    download_project(PROJ boost
-            GIT_REPOSITORY https://github.com/boostorg/boost.git
-            GIT_TAG        boost-1.69.0
-            UPDATE_DISCONNECTED 1
-            QUIET
-            )
 
-else ()
-    include(FetchContent)
-    FetchContent_Declare(boost
-            GIT_REPOSITORY https://github.com/boostorg/boost.git
-            GIT_TAG        boost-1.69.0
-            )
-    FetchContent_GetProperties(boost)
-    if (NOT boost_POPULATED)
-        FetchContent_Populate(boost)
-    endif ()
-endif ()
+include(FetchContent)
+FetchContent_Declare(boost
+            GIT_REPOSITORY      https://github.com/boostorg/boost.git
+            GIT_TAG             boost-1.69.0
+            CONFIGURE_COMMAND   "echo installing"
+        )
+FetchContent_GetProperties(boost)
+
+if(NOT boost_POPULATED)
+    FetchContent_Populate(boost)
+endif()
+
+execute_process(
+        COMMAND "echo installing && ./boostrap.sh && ./b2 headers"
+        WORKING_DIRECTORY ${boost_SOURCE_DIR})
 
 
 set(Boost_INCLUDE_DIRS ${boost_SOURCE_DIR}/include/ CACHE DIRECTORY "Include Directory of the OpneDRIVE parser" FORCE)
