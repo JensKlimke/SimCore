d5835846edc775112559c6d151d322f16d595622
Jens Klimke <jens.klimke@rwth-aachen.de>
2020-10-01 23:29:46 +0200

    testing framework

---
+++ src/include/simcore/testing/SimulationTest.h
@@ -0,0 +1,245 @@
+// Copyright (c) 2020 Jens Klimke (jens.klimke@rwth-aachen.de). All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Created by Jens Klimke on 2020-10-01.
+//
+
+
+#ifndef SIMCORE_SIMULATION_H
+#define SIMCORE_SIMULATION_H
+
+#include <memory>
+#include <vector>
+#include <functional>
+#include "../Loop.h"
+#include "../Model.h"
+#include "../timers/RealTimeTimer.h"
+#include "../timers/TimeIsUp.h"
+#include "../logging/TimeReporter.h"
+#include "../value/ValueExceed.h"
+
+namespace sim::testing {
+
+    template<class T>
+    class SimulationTest : public Loop, public T {
+
+        std::unique_ptr<sim::BasicTimer> _timer{};
+        std::vector<std::unique_ptr<sim::IComponent>> _components{};
+
+    public:
+
+        struct Time{
+            double time;
+            double deltaTime;
+            double initTime;
+            double termTime;
+            unsigned long steps;
+        };
+
+
+        /**
+         * Creates a component, registers it and takes responsibility to destroy
+         * @tparam Comp Component type
+         * @tparam Args Construction arguments types
+         * @param args Arguments
+         */
+        template<class Comp, class ...Args>
+        Comp *createComponent(Args &&...args) {
+
+            // create component and add to component list
+            auto comp = new Comp(std::forward<Args>(args)...);
+            _components.emplace_back(comp);
+
+            // add component
+            this->addComponent(comp);
+
+            // return component
+            return comp;
+
+        }
+
+
+        /**
+         * Creates the simulation
+         * @param endTime Time at which the simulation shall end
+         * @param timeStepSize Time step size
+         * @param startTime Time at which the simulation shall start, default: 0.0
+         * @param acceleration The acceleration to real-time, the simulation shall be executed, default = INFINITY
+         */
+        void create(double endTime, double timeStepSize, double startTime = INFINITY, double acceleration = INFINITY) {
+
+            // create timer
+            if(std::isinf(acceleration)) {
+
+                // create and save basic timer
+                _timer = std::make_unique<sim::BasicTimer>();
+
+            } else {
+
+                // create real-time timer
+                auto timer = new sim::RealTimeTimer;
+                timer->setAcceleration(acceleration);
+
+                // save timer
+                _timer.reset(timer);
+
+                // set time reporter
+                auto timeReporter = createComponent<sim::logging::TimeReporter>();
+                timeReporter->setTimeStepSize(1.0);
+
+            }
+
+            // setup timer
+            _timer->setTimeStepSize(timeStepSize);
+            _timer->setStartTime(startTime);
+
+            // add to loop
+            setTimer(_timer.get());
+
+            // stop condition (time)
+            auto stopTimer = createComponent<sim::TimeIsUp>();
+            stopTimer->setStopTime(endTime);
+
+            // add component as stop condition
+            addStopCondition(stopTimer);
+
+            // add myself to the loop
+            this->addComponent(this);
+
+        }
+
+
+        /**
+         * Returns the time structure
+         * @return Time structure
+         */
+        const Time &time() {
+
+            return _time;
+
+        }
+
+
+        /**
+         * Creates stop condition based on a given pointer (if value is exceed, the simulation stops)
+         * @tparam VType Type of the value to be checked
+         * @param stopValues A pair of time and limit
+         * TODO: add stop mode
+         */
+        template<typename VType>
+        void stopExceed(const std::pair<double, VType*> &stopValues) {
+
+            // create stop condition
+            auto b = createComponent<sim::value::ValueExceed<VType>>();
+            b->setPointerAndLimit(stopValues.first, stopValues.second);
+
+            // add stop condition
+            addStopCondition(b);
+
+        }
+
+
+        /**
+         * Adds a callback function to be executed before the execution step of this component is executed
+         * @param fnc Callback function
+         */
+        void addPreCallback(std::function<void()> &&fnc) {
+
+            // add to pre steps
+            _preSteps.emplace_back(std::move(fnc));
+
+        }
+
+
+        /**
+         * Adds a callback function to be executed before the execution step of this component is executed
+         * @param fnc Callback function
+         */
+        void addPostCallback(std::function<void()> &&fnc) {
+
+            // add to pre steps
+            _postSteps.emplace_back(std::move(fnc));
+
+        }
+
+
+    protected:
+
+        Time _time;
+
+        std::vector<std::function<void()>> _preSteps{};
+        std::vector<std::function<void()>> _postSteps{};
+
+
+        void initialize(double t) override {
+
+            // call super init
+            T::initialize(t);
+
+            // set initialization time
+            _time.initTime = t;
+
+            // reset every other time elements
+            _time.termTime = INFINITY;
+            _time.time = 0.0;
+            _time.deltaTime = 0.0;
+            _time.steps = 0;
+
+        }
+
+
+        void step(double t, double dt) override {
+
+            // get time step size and save time
+            _time.deltaTime = dt;
+            _time.time = t;
+
+            // run pre-steps
+            for (auto &ps : _preSteps)
+                ps();
+
+            // run main step
+            T::step(t, dt);
+
+            // run post-steps
+            for (auto &ps : _postSteps)
+                ps();
+
+            // increment steps
+            _time.steps++;
+
+        }
+
+
+        void terminate(double t) override {
+
+            // set termination time
+            _time.termTime = t;
+
+            // terminate super
+            T::terminate(t);
+
+        }
+
+    };
+
+}
+
+#endif // SIMCORE_SIMULATION_H
---
+++ test/cucumber/features/step_definitions/VehicleSimulation.h
@@ -0,0 +1,106 @@
+// Copyright (c) 2020 Jens Klimke.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Created by Jens Klimke on 2020-09-30.
+// Contributors:
+//
+
+
+#ifndef SIMCORE_SIM_TEST_H
+#define SIMCORE_SIM_TEST_H
+
+#include <simcore/testing/SimulationTest.h>
+#include <simtraffic/VehicleModelAdapter.h>
+
+#ifndef TIME_STEP_SIZE
+#define TIME_STEP_SIZE 0.01
+#endif
+
+auto const D = sim::traffic::VehicleModel::ShifterPosition::DRIVE;
+auto const P = sim::traffic::VehicleModel::ShifterPosition::PARK;
+auto const N = sim::traffic::VehicleModel::ShifterPosition::NEUTRAL;
+auto const R = sim::traffic::VehicleModel::ShifterPosition::REVERSE;
+
+
+struct VehicleSimulation : public sim::testing::SimulationTest<sim::traffic::VehicleModelAdapter> {
+
+
+    sim::traffic::VehicleModel::State *_state{};
+    sim::traffic::VehicleModel::Input *_input{};
+    sim::traffic::VehicleModel::Parameters *_params{};
+
+    VehicleSimulation() {
+
+        using namespace sim::traffic;
+
+        // save state
+        this->_state = &state;
+        this->_input = &input;
+        this->_params = &parameters;
+
+        // set initial state
+        state.xPosition = 0.0;
+        state.yPosition = 0.0;
+        state.velocity = 0.0;
+        state.acceleration = 0.0;
+        state.yawAngle = 0.0;
+        state.yawRate = 0.0;
+
+        // reset forces
+        parameters.externalRelForce = 0.0;
+        parameters.resistanceParameters[0] = 0.0;
+        parameters.resistanceParameters[1] = 0.0;
+        parameters.resistanceParameters[2] = 0.0;
+
+        // set parameters parameters
+        parameters.maxRelReverseDrivePower =  10.0;
+        parameters.maxRelDrivePower        = 100.0;
+        parameters.maxRelDriveTorque       =  10.0;
+        parameters.maxRelBrakeTorque       =  10.0;
+        parameters.maxCurvature            =   0.1;
+
+        // reset inputs
+        input.drive = 0.0;
+        input.brake = 0.0;
+        input.steering = 0.0;
+
+    }
+
+    void execute(double time) {
+
+        // create simulation
+        create(time, TIME_STEP_SIZE, false);
+
+        // set time step size
+        setTimeStepSize(TIME_STEP_SIZE);
+
+        // add
+        addPreCallback([this]() {
+            
+        });
+
+        // run simulation
+        run();
+
+    }
+
+};
+
+#endif // SIMCORE_SIM_TEST_H
