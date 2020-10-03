2fe56046fcc297fd4081c70e9b63334544921e84
Jens Klimke <jens.klimke@rwth-aachen.de>
2020-10-03 11:28:17 +0200

    Added protobuf test

--- test/protobuf/ProtoTest.cpp
+++ test/protobuf/ProtoTest.cpp
@@ -1,5 +1,4 @@
-//
-// Copyright (c) 2019-2020 Jens Klimke <jens.klimke@rwth-aachen.de>
+// Copyright (c) 2020 Jens Klimke.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,32 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-// Created by Jens Klimke on 2019-11-06.
+// Created by Jens Klimke on $date.get('yyyy-M-d').
+// Contributors:
 //
 
-#ifndef SIMCORE_FUNCTIONS_H
-#define SIMCORE_FUNCTIONS_H
 
-#include <string>
-
-namespace sim::fnc {
-
-    /**
-     * String formatter
-     * @tparam Args
-     * @param format Format string
-     * @param args Inputs
-     * @return Formatted string
-     */
-    template<typename ... Args>
-    std::string string_format(const std::string &format, Args ... args) {
-        auto size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
-        char *buf(new char[size]);
-        snprintf(buf, size, format.c_str(), args ...);
-        return std::string(buf, buf + size - 1); // We don't want the '\0' inside
-    }
-
-
-} // namespace ::sim::fnc
-
-#endif //SIMCORE_FUNCTIONS_H
