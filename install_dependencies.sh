ad076acd94eabddcda7df6b2c79eb507f0374a5d
Jens Klimke <jens.klimke@rwth-aachen.de>
2021-05-08 17:27:35 +0200

    downgraded gtest

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 mkdir third_party
 cd third_party
-git clone --branch "release-1.10.0" --recursive https://github.com/google/googletest.git
+git clone --branch "release-1.8.0" --recursive https://github.com/google/googletest.git
 cd googletest
 cmake -DCMAKE_INSTALL_PREFIX=../../install CMakeLists.txt
 make
